c44ac9f89f84e31a593a991e659f9fd31d0a495f
Rafael <rafaeldealmeida.192001@gmail.com>
2023-02-16 00:02:17 -0300

    Pegando o grafico da planilha

--- planilha.py
+++ planilha.py
@@ -1,6 +1,18 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 x = pd.read_excel(r"C:\Users\Rafael.Almeida\Desktop\Aula\Alura\Python\Pandas\lendo_planilhas\teste.xlsx")
 
+# Grafico em Linha
+# plt.plot(x["Nome"])
+
+# Grafico em Histograma
+# plt.hist("ID", bins= 20)
+
+# Grafico em formato de pizza
+plt.pie(x["ID"], labels=x["Nome"], autopct="%1.0f%%")
+
+plt.show()
+
 # print(x)
-print(x["Nome"][7])
+# print(x["Nome"][7])
